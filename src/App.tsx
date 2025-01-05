5253ab74f378b63ddee703f8fdbd9830f744a19d
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-01-05 23:47:05 +0600

    add: useContext hook implemented in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
 import './App.css';
 import Button from './Button';
 import Counter from './Counter';
 import Counter2 from './Counter2';
 import Greeting from './Greeting';
 
+interface UserContextType {
+  name: string;
+  setName: (name: string) => void;
+}
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+const UserProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [name, setName] = useState<string>('Thomas');
+  return (
+    <UserContext.Provider value={{ name, setName }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const DisplayName: React.FC = () => {
+  const user = useContext(UserContext);
+  if (!user) throw new Error('DisplayName must be used within UserProvider');
+  return <p>Name:{user.name}</p>;
+};
+
 function App() {
   const handleClick = () => alert('Button clicked');
   return (
     <>
-      <div>
-        <Greeting name="Thomas" age={69}></Greeting>
-        <Button label="Click Me" onClick={handleClick}></Button>
-        <Counter />
-        <Counter2 />
-      </div>
+      <UserProvider>
+        <div>
+          <Greeting name="Thomas" age={69}></Greeting>
+          <Button label="Click Me" onClick={handleClick}></Button>
+          <Counter />
+          <Counter2 />
+          <DisplayName></DisplayName>
+        </div>
+      </UserProvider>
     </>
   );
 }
